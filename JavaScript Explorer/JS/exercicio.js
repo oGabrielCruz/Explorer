b719e55427685056c9e7a967bafb5eb69e899898
Gabriel da Silva Cruz <gabrielsc05@outlook.com>
2023-07-11 11:09:15 -0300

    .

---
+++ JavaScript Explorer/JS/exercicio.js
@@ -0,0 +1,64 @@
+//1 
+
+// let weight;
+
+//2
+
+// console.log(typeof weight)
+
+//3
+
+// let name = "Biel"
+// let age = 17
+// let stars =  4.5
+// let isSubscribed = True
+
+//4
+
+// student = {
+//     name: "Biel",
+//     age: 17,
+//     stars:  4.5,
+//     isSubscribed: true
+// }
+// console.log(student) 
+
+//4.1
+
+//  student = {
+//      name: 'Biel',
+//      age: 17,
+//      weigth:  4.5,
+//  }
+//  console.log(`O ${student.name} tem ${student.age} anos e pesa ${student.weigth} kg`) 
+
+//5
+
+// let student = []
+
+//6
+
+// students = [
+//     student
+// ]
+// console.log(student)
+
+//7 
+
+// console.log(students[0])
+
+//8
+
+// const biel = [
+//     name='Biel',
+//     age=17,
+//     weigth= 4.8
+// ]
+
+// console.log(biel[0])
+
+//9
+
+console.log(a)
+var a = 1
+console.log('Vai dar Undefined pq o var é global, se não dava erro')
