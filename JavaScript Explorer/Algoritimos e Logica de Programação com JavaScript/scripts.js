30318f803aacff9c4d724aed630c57b8faa2b610
Gabriel da Silva Cruz <gabrielsc05@outlook.com>
2023-07-25 11:46:21 -0300

    Alert

---
+++ JavaScript Explorer/Algoritimos e Logica de Programação com JavaScript/scripts.js
@@ -0,0 +1,9 @@
+/* 
+    Encontre a solução do Problema:
+
+    Pergunte ao usuário e escreva a mensagem:
+    "Olá, [Nome do usuário]"
+*/
+
+let nome = prompt('Qual é o seu nome?')
+alert('Olá, ' + nome )
