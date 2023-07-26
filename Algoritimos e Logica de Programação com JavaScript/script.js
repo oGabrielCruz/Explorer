576712850fe90580de411c7917d69e036643442d
Gabriel da Silva Cruz <gabrielsc05@outlook.com>
2023-07-26 12:19:03 -0300

    .

---
+++ Algoritimos e Logica de Programação com JavaScript/script.js
@@ -0,0 +1,8 @@
+/* 
+    Soma de 2 numeros
+*/
+
+alert('Vamos fazer a soma de dois numeros ')
+let numberOne = prompt('Digite o primeiro valor ')
+let numberTwo = prompt('Digite o segundo valor ')
+alert("Resultado Final: " +(Number(numberOne) + Number(numberTwo)))
