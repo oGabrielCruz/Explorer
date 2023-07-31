e007a3d95f6463d8a8916f6e1a67badebe80a7ac
Gabriel da Silva Cruz <gabrielsc05@outlook.com>
2023-07-31 15:23:38 -0300

    Aprendendo For

---
+++ Algoritimos e logica de programação/04/scripts.js
@@ -0,0 +1,11 @@
+/*
+    Capture 10 items para compor a lista de um supermercado
+
+    Após capturar os 10 itens, imprima-os, separando por vírgula
+*/
+
+for(let item = 0;
+    item < 10; 
+    item++){
+
+}
