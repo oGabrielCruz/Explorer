371d2b8542525efd997523dadf446423c385e583
Gabriel da Silva Cruz <gabrielsc05@outlook.com>
2023-08-01 12:10:12 -0300

    Lista de Compras

--- Algoritimos e logica de programação/04/scripts.js
+++ Algoritimos e logica de programação/04/scripts.js
@@ -4,8 +4,10 @@
     Após capturar os 10 itens, imprima-os, separando por vírgula
 */
 
-for(let item = 0;
-    item < 10; 
-    item++){
+let items = [] //array
 
+for (let item = 0; item < 10; item++ ){
+    let lista = prompt("Digite o item " + (item + 1))
+    items[item] = lista
 }
+alert(items)
