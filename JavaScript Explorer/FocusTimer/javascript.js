493f5344a85daa6d643cc1a016ff5ba6062a0b16
Gabriel da Silva Cruz <gabrielsc05@outlook.com>
2024-01-15 15:40:49 -0300

    cronometro feito

--- JavaScript Explorer/FocusTimer/javascript.js
+++ JavaScript Explorer/FocusTimer/javascript.js
@@ -1,3 +1,5 @@
+var sec=0
+var min=0
 function tema() {
     const elementoFundo = document.getElementById("fundo");
     console.log('WTF é elementoFundo: ', elementoFundo)
@@ -6,9 +8,46 @@ function tema() {
     if (elementoFundo.style.background == "rgb(0, 0, 0)" || !elementoFundo.style.background){
         elementoFundo.style.background="#ffffff"
     }
-    else(
+    else (
         elementoFundo.style.background= "#000000"
     )
     console.log("Olha isso biel 2: ", elementoFundo.style.background)
+
+}
+
+
+function start(){
+    console.log('começo')
+    interval=setInterval(timer,100)
+    
+}
+function pause(){
+    clearInterval(interval)
+}
+function parar(){
+    clearInterval(interval)
+    min=0
+    sec=0
+    document.getElementById('timer').innerText='00:00'
+}
+
+
+
+function timer(){
+    sec++
+    document.getElementById('timer').innerText=doisDigitos(min)+':'+doisDigitos(sec)
+    if(sec==60){
+        min++
+        sec=0
+    }
+}
+
+function doisDigitos(digito){
+    if(digito < 10){
+        return('0'+digito)
+    }
+    else{
+        return(digito)
+    }
 
 }
